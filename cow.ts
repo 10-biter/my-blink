053be0210993acd2439b0daefe929f111cc02aaf
10-biter <ep.student@hotmail.com>
2025-11-19 19:01:40 +0300

    Update pxt.json, custom.ts, cow.ts

---
+++ cow.ts
@@ -0,0 +1,15 @@
+namespace bliks {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% block
+    export function plick(x: number, y: number, ms: number): void {
+        basic.forever(function () {
+            led.toggle(x, y)
+            basic.pause(ms)
+        })
+    }
+}
--- custom.ts
+++ custom.ts
@@ -15,17 +15,3 @@ enum MyEnum {
  * Custom blocks
  */
 //% weight=100 color=##6c008a icon="/uf600"
-namespace bliks {
-    /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
-     */
-    //% block
-    export function plick(x: number, y:number, ms:number): void{
-        basic.forever(function() {
-            led.toggle(x, y)
-            basic.pause(ms)
-        })
-}}
