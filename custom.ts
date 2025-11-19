1c5a792cfadebc36d77e81b05c1aead3bd8bc257
10-biter <ep.student@hotmail.com>
2025-11-19 17:55:17 +0300

    Update pxt.json, custom.ts

--- custom.ts
+++ custom.ts
@@ -15,7 +15,7 @@ enum MyEnum {
  * Custom blocks
  */
 //% weight=100 color=#0fbc11 icon=""
-namespace custom {
+namespace rebet {
     /**
      * TODO: describe your function here
      * @param n describe parameter here, eg: 5
