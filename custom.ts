990dbdd93b0f511f264a0be2838d79d261064049
10-biter <ep.student@hotmail.com>
2025-11-19 18:59:49 +0300

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -14,7 +14,7 @@ enum MyEnum {
 /**
  * Custom blocks
  */
-//% weight=100 color=##6c008a icon="[]"
+//% weight=100 color=##6c008a icon="/uf600"
 namespace bliks {
     /**
      * TODO: describe your function here
